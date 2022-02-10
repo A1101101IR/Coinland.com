832a2663fd924ba423d9d41a5804a9fe0aba5152
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-02-10 11:14:29 +0100

    added mixins and variables

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
-const About = () => {
-  return (
-    <section className="about">
-      {/* what about use section for each part of page to simplify html structure?  */}
-      <h1>this is about page</h1>
-    </section>
-  );
-};
+import "./about.scss"
 
-export default About;
+const About = () => {
+	return (
+		<section className='about'>
+			{/* what about use section for each part of page to simplify html structure?  */}
+			<h1>this is about page</h1>
+		</section>
+	)
+}
+
+export default About
