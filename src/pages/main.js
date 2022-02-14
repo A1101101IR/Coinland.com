2b407458bfbc5476608209f6c94fd893b0d83e0c
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-02-14 15:09:38 +0100

    addded main page sectipn

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,20 @@
-const Main = () => {
-  return (
-    <section className="main">
-      {/* what about use section for each part of page to simplify html structure?  */}
-      <h1>this is main page</h1>
-    </section>
-  );
-};
+import "./main.scss"
 
-export default Main;
+const Main = () => {
+	return (
+		<div className='main__container'>
+			{/* what about use section for each part of page to simplify html structure?  */}
+			<section className='header'>
+				<div className='blueblob'></div>
+				<h1>What happens in blockchain, stays in blockchain.</h1>
+				<p>
+					You can’t ignore the blockchain anymore. You’d better start
+					learning it. The time is now.
+				</p>
+				<div className='greenblob'></div>
+			</section>
+		</div>
+	)
+}
+
+export default Main
