d270bfd6242ff27f44279def67f598aa1030844e
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-02-15 19:28:57 +0100

    added service section

---
+++ src/components/service.js
@@ -0,0 +1,26 @@
+const Service = () => {
+  return (
+    <section className="service">
+      <div className="service-box">
+        <h2>Online Support 24/7</h2>
+        {/* <h1>Coinland</h1> */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero,
+          voluptatum? Perferendis, officia non. Explicabo minima dolore, velit
+          voluptas facere, ut ea animi sunt harum optio mollitia similique,
+          ipsam earum hic!
+        </p>
+        <div className="service-person-contact">
+          <div className="service-img"></div>
+          <div className="service-personal">
+            <h4>Sven Svensson</h4>
+            <h4>Sven@Coinland.com</h4>
+          </div>
+        </div>
+        <button>Contact</button>
+      </div>
+    </section>
+  );
+};
+
+export default Service;
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,12 +3,14 @@ import cryptoscreen from "../img/screen.png";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Pricing from "../components/pricing";
+import Service from "../components/service";
 
 const Main = () => {
   return (
     <div className="main__container">
       {/* what about use section for each part of page to simplify html structure?  */}
       <Header />
+      <Service />
       <Pricing />
       <Footer />
     </div>
