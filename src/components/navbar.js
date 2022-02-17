0005de54b5afe6af4f7eea3d04605120e8331f36
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-02-17 13:51:29 +0100

    updated login button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,97 +1,115 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import openbtn from "../img/open-btn.png";
-import closebtn from "../img/close-btn.png";
+import React from "react"
+import { Link } from "react-router-dom"
+import openbtn from "../img/open-btn.png"
+import closebtn from "../img/close-btn.png"
+import metamask from "../img/metamask.png"
 class Navbar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      open: false,
-    };
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-  handleOpen() {
-    this.setState({
-      open: true,
-    });
-  }
-  handleClose() {
-    this.setState({
-      open: false,
-    });
-  }
-  render() {
-    return (
-      <section className="navbar">
-        <div className="nav-left">
-          <Link className="logo" to="/">
-            <h2>Coinland</h2>
-          </Link>
-        </div>
-        {!this.state.open && (
-          <nav className="nav-center">
-            <Link className="link" to="/">
-              Home
-            </Link>
-            <Link className="link" to="/about">
-              Product
-            </Link>
-            <Link className="link" to="/about">
-              About
-            </Link>
-            <Link className="link" to="/contact">
-              Contact
-            </Link>
-          </nav>
-        )}
-        <div className="nav-right">
-          <Link className="login-btn" to="/">
-            Login
-          </Link>
-          {!this.state.open && (
-            <img
-              className="open-btn"
-              onClick={this.handleOpen}
-              src={openbtn}
-              width={36}
-            />
-          )}
-        </div>
-        {this.state.open && (
-          <div className="toggle-nav">
-            <img
-              className="close-btn"
-              onClick={this.handleClose}
-              src={closebtn}
-              width={36}
-            />
-            <ul>
-              <li>
-                <Link onClick={this.handleClose} className="link" to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link onClick={this.handleClose} className="link" to="/about">
-                  Product
-                </Link>
-              </li>
-              <li>
-                <Link onClick={this.handleClose} className="link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link onClick={this.handleClose} className="link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        )}
-      </section>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			open: false,
+		}
+		this.handleOpen = this.handleOpen.bind(this)
+		this.handleClose = this.handleClose.bind(this)
+	}
+	handleOpen() {
+		this.setState({
+			open: true,
+		})
+	}
+	handleClose() {
+		this.setState({
+			open: false,
+		})
+	}
+	render() {
+		return (
+			<section className='navbar'>
+				<div className='nav-left'>
+					<Link className='logo' to='/'>
+						<h2>Coinland</h2>
+					</Link>
+				</div>
+				{!this.state.open && (
+					<nav className='nav-center'>
+						<Link className='link' to='/'>
+							Home
+						</Link>
+						<Link className='link' to='/about'>
+							Product
+						</Link>
+						<Link className='link' to='/about'>
+							About
+						</Link>
+						<Link className='link' to='/contact'>
+							Contact
+						</Link>
+					</nav>
+				)}
+				<div className='nav-right'>
+					<Link className='login-btn' to='/'>
+						<img src={metamask} alt='' width={30} /> Connect with
+						MetaMask
+					</Link>
+					{!this.state.open && (
+						<img
+							className='open-btn'
+							onClick={this.handleOpen}
+							src={openbtn}
+							width={36}
+						/>
+					)}
+				</div>
+				{this.state.open && (
+					<div className='toggle-nav'>
+						<img
+							className='close-btn'
+							onClick={this.handleClose}
+							src={closebtn}
+							width={36}
+						/>
+						<ul>
+							<li>
+								<Link
+									onClick={this.handleClose}
+									className='link'
+									to='/'
+								>
+									Home
+								</Link>
+							</li>
+							<li>
+								<Link
+									onClick={this.handleClose}
+									className='link'
+									to='/about'
+								>
+									Product
+								</Link>
+							</li>
+							<li>
+								<Link
+									onClick={this.handleClose}
+									className='link'
+									to='/about'
+								>
+									About
+								</Link>
+							</li>
+							<li>
+								<Link
+									onClick={this.handleClose}
+									className='link'
+									to='/contact'
+								>
+									Contact
+								</Link>
+							</li>
+						</ul>
+					</div>
+				)}
+			</section>
+		)
+	}
 }
-export default Navbar;
+export default Navbar
