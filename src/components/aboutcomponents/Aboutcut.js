1952e280bb516bf19e53ea3490c7dff9c6cb6c5d
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-02-22 12:50:51 +0100

    added react icons and about page

--- src/components/aboutcomponents/Aboutcut.js
+++ src/components/aboutcomponents/Aboutcut.js
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom"
 import "./Aboutcut.scss"
 
-const Aboutcut = () => {
+const Aboutcut = ({ text, button, path, background }) => {
 	return (
-		<section className='aboutcut'>
+		<section className='aboutcut' style={{ backgroundColor: background }}>
 			<div className='aboutcut__container'>
-				<h1>“Make smooth transactions with MetaMask”</h1>
-				<button>Dashboard</button>
+				<h1>{text}</h1>
+				<Link to={path}>
+					<button>{button}</button>
+				</Link>
 			</div>
 		</section>
 	)
