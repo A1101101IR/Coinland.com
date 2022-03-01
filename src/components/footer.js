676cda0ba373d86bafd09d01297235e15e6751fd
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-03-01 16:06:50 +0100

    updated UI

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,57 @@
-import logoimage from "../img/logo.png";
-import { Link } from "react-router-dom";
-import Twitter from "../img/twitter.png";
-import Facebook from "../img/facebook.png";
-import Instagram from "../img/instagram.png";
+import logoimage from "../img/logo.png"
+import { Link } from "react-router-dom"
+import { BsFacebook, BsGithub, BsTwitter } from "react-icons/bs"
 
 const Footer = () => {
-  return (
-    <section className="Footer">
-      <div className="coinland-footer">
-        <Link className="logo" to="/">
-          <img src={logoimage} width={40} alt="" />
-          <h2>Coinland.</h2>
-        </Link>
-      </div>
-      <div>
-        <nav className="footer-nav">
-          <Link className="link" to="/">
-            Home
-          </Link>
-          <Link className="link" to="/product">
-            Product
-          </Link>
-          <Link className="link" to="/about">
-            About
-          </Link>
-          <Link className="link" to="/contact">
-            Contact
-          </Link>
-        </nav>
-      </div>
-      <div className="social-icons">
-        <a href="https://www.facebook.com" target="_blank">
-          <img src={Facebook} width={40} />
-        </a>
-        <a href="https://www.instagram.com" target="_blank">
-          <img src={Instagram} width={40} />
-        </a>
-        <a href="https://twitter.com" target="_blank">
-          <img src={Twitter} width={40} />
-        </a>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className='Footer'>
+			<div className='coinland-footer'>
+				<Link className='logo' to='/'>
+					<img src={logoimage} width={40} alt='' />
+					<h2>Coinland.</h2>
+				</Link>
+			</div>
+			<div>
+				<nav className='footer-nav'>
+					<Link className='link' to='/'>
+						Home
+					</Link>
+					<Link className='link' to='/product'>
+						Product
+					</Link>
+					<Link className='link' to='/about'>
+						About
+					</Link>
+					<Link className='link' to='/contact'>
+						Contact
+					</Link>
+				</nav>
+			</div>
+			<div className='social-icons'>
+				<a
+					href='https://www.facebook.com'
+					target='_blank'
+					className='linkssocial'
+				>
+					<BsGithub />
+				</a>
+				<a
+					href='https://www.instagram.com'
+					target='_blank'
+					className='linkssocial'
+				>
+					<BsTwitter />
+				</a>
+				<a
+					href='https://twitter.com'
+					target='_blank'
+					className='linkssocial'
+				>
+					<BsFacebook />
+				</a>
+			</div>
+		</section>
+	)
+}
 
-export default Footer;
+export default Footer
--- src/components/news.js
+++ src/components/news.js
@@ -1,56 +1,51 @@
 const News = () => {
-  return (
-    <section className="news">
-      <div className="bg bg-one">
-        <div className="news-box">
-          <div className="news-one">
-            <div className="news-text-box">
-              <h2>
-                Bitcoin ETFs Remain Popular Among Investors Despite Price Slump
-              </h2>
-              <p>
-                Bitcoin (BTC) could rise to a high of USD 93,000 or more this
-                year, and close to USD 193,000 by the end of 2025, a panel of
-                industry players gathered by comparison website Finder
-                predicted. However, they were less optimistic than in October.{" "}
-                <br /> <br />
-                By averaging out responses from the panelists, Finder came up
-                with a price prediction of USD 93,717 as bitcoin’s high for
-                2022, with the coin expected to end the year at USD 76,360.
-              </p>
-              <span>
-                <button>LEARN MORE...</button>
-              </span>
-            </div>
-            <div className="img-box"></div>
-          </div>
-        </div>
-      </div>
-      <div className="bg bg-two">
-        <div className="news-box">
-          <div className="news-two">
-            <div className="img-box"></div>
-            <div className="news-text-box">
-              <h2>Bitcoin and Ethereum Consolidate Gains, SOL Inches Higher</h2>
-              <p>
-                Bitcoin (BTC) could rise to a high of USD 93,000 or more this
-                year, and close to USD 193,000 by the end of 2025, a panel of
-                industry players gathered by comparison website Finder
-                predicted. However, they were less optimistic than in October.{" "}
-                <br /> <br />
-                By averaging out responses from the panelists, Finder came up
-                with a price prediction of USD 93,717 as bitcoin’s high for
-                2022, with the coin expected to end the year at USD 76,360.
-              </p>
-              <span>
-                <button>LEARN MORE...</button>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+	return (
+		<section className='news'>
+			<div className='bg bg-one'>
+				<div className='news-box'>
+					<div className='news-one'>
+						<div className='news-text-box'>
+							<h2>Bitcoin Remain Popular Among Investors</h2>
+							<p>
+								Bitcoin (BTC) could rise to a high of USD 93,000
+								or more this year, and close to USD 193,000 by
+								the end of 2025, a panel of industry players
+								gathered by comparison website Finder predicted.
+								However, they were less optimistic than i...
+							</p>
+							<span>
+								<button>LEARN MORE...</button>
+							</span>
+						</div>
+						<div className='img-box'></div>
+					</div>
+				</div>
+			</div>
+			<div className='bg bg-two'>
+				<div className='news-box'>
+					<div className='news-two'>
+						<div className='img-box'></div>
+						<div className='news-text-box'>
+							<h2>
+								Bitcoin and Ethereum Consolidate Gains, SOL
+								Inches Higher
+							</h2>
+							<p>
+								Bitcoin (BTC) could rise to a high of USD 93,000
+								or more this year, and close to USD 193,000 by
+								the end of 2025, a panel of industry players
+								gathered by comparison website Finder predicted.
+								However, they we...
+							</p>
+							<span>
+								<button>LEARN MORE...</button>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
 
-export default News;
+export default News
