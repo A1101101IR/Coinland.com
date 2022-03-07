97de96963563bfd2a9e3308b20906f4e85f3b406
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-03-07 13:56:55 +0100

    updated link

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,22 @@
 import Dashboard from "../pages/dashboard"
 import macbook from "../img/MacBook.png"
+import { Link } from "react-router-dom"
 const Header = () => {
 	return (
 		<section className='header'>
 			<div className='blueblob'></div>
 			<h1>What happens in blockchain, stays in blockchain.</h1>
 			<p>
-				You can’t ignore the blockchain anymore. You’d better start
-				learning it. The time is now.
+				We have the worlds best Crypto Dashboard and its free to use
+				today! <br />
+				Check it out below!
 			</p>
 			<div className='greenblob'></div>
 			<div className='split'>
 				<div className='active'>
-					<button className='active'>Get started</button>
+					<Link to='/dashboard'>
+						<button className='active'>Get started</button>
+					</Link>
 				</div>
 				<div className='outline'>
 					<button className='outline'>Try for free</button>
