8202a768273074a3e9da9f0842a108544f094c40
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-03-14 19:40:42 +0100

    added crypto news api to news section

--- src/components/news.js
+++ src/components/news.js
@@ -1,45 +1,51 @@
+import {useEffect, useState} from 'react'
 const News = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("https://free-news.p.rapidapi.com/v1/search?q=bitcoin%2C%20ethereum&lang=en&page=1", {
+	  "method": "GET",
+	  "headers": {
+		"x-rapidapi-host": "free-news.p.rapidapi.com",
+		"x-rapidapi-key": "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901"
+	}
+  })
+  .then(response => response.json())
+  .then((data) => setData(data.articles))
+  .catch(err => {
+	  console.error(err);
+  });
+  }, [])
+
   return (
-    <section className="news">
-      <div className="bg bg-one">
-        <div className="news-box">
-          <div className="news-one">
+    <div className='news'>
+      {/* if there is data map each obj */}
+      {data && data.slice(0, 3).map((art) => (
+        <div key={art.id} className='news-box'>
+          {/* changes our template depending on index of object.  */}
+          {(data.indexOf(art) % 2 == 0) ? <>
             <div className="news-text-box">
-              <h2>Bitcoin Remain Popular Among Investors</h2>
-              <p>
-                Bitcoin (BTC) could rise to a high of USD 93,000 or more this
-                year, and close to USD 193,000 by the end of 2025, a panel of
-                industry players gathered by comparison website Finder
-                predicted. However, they were less optimistic than i...
-              </p>
-              <span>
-                <button>LEARN MORE...</button>
-              </span>
+              <div>
+                <h3>{art.title}</h3>
+                <pre>{art.author}</pre><pre>{art.published_date}</pre>
+              </div>
+              <p>{art.summary.substring(0, 245)}</p>
+              <span><a href={art.link} target="_blank"><button>Read more</button></a></span>
             </div>
-            <div className="img-box"></div>
-          </div>
-        </div>
-      </div>
-      <div className="bg bg-two">
-        <div className="news-box">
-          <div className="news-two">
-            <div className="img-box"></div>
+            <img src={art.media} alt="" className='news-img' />
+            </>:<>
+            <img src={art.media} alt="" className='news-img' />
             <div className="news-text-box">
-              <h2>Bitcoin and Ethereum Consolidate Gains, SOL Inches Higher</h2>
-              <p>
-                Bitcoin (BTC) could rise to a high of USD 93,000 or more this
-                year, and close to USD 193,000 by the end of 2025, a panel of
-                industry players gathered by comparison website Finder
-                predicted. However, they we...
-              </p>
-              <span>
-                <button>LEARN MORE...</button>
-              </span>
+              <div>
+                <h3>{art.title}</h3>
+                <pre>{art.author}</pre><pre>{art.published_date}</pre>
+              </div>
+              <p>{art.summary.substring(0, 245)}</p>
+              <span><a href={art.link} target="_blank"><button>Read more</button></a></span>
             </div>
-          </div>
+            </>}
         </div>
-      </div>
-    </section>
+      ))}
+    </div>
   );
 };
 
