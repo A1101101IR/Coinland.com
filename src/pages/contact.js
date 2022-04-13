3a9fefbeee0264ed49de503800cbde1e28eaeadf
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-04-13 19:55:06 +0200

    small fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import Contactform from "../components/Contactform";
 import ContactHeader from "../components/ContactHeader";
-
+import ChartPage from "../pages/dashpage";
 const Contact = () => {
   return (
     <section className="contact__main">
